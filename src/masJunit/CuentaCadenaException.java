7b1b5b498dc7ac83de722ae3a1b9e0ad20489095
GIT_RUBENGOMEZ <rgomez@gtec.es>
2022-06-04 16:28:21 +0200

    Creación de nueva rama con los test de excepciones de Junit

---
+++ src/masJunit/CuentaCadenaException.java
@@ -0,0 +1,25 @@
+package cuentacadenas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CuentaCadenaException {
+	
+String Exception;
+	@Test
+	public static void contarCadenasException (String args[]) {
+		try {
+			
+		} catch (Exception e) {
+			
+		}
+
+		
+	}
+	@Test
+	public void test() {
+		
+	}
+
+}
